41055a5e527c4f735b473ae8ffc1add7fe8c9b4d
mayr0y <alexlzrw@gmail.com>
2023-02-16 00:04:51 +0300

    wip: added handlers, endpoints

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"log"
 	specialtribble "special-tribble"
+	"special-tribble/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(specialtribble.Server)
-	err := srv.Run("8000")
-	if err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
